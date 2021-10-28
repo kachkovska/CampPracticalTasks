40d716641d7e29c730b94bd3d5fcb6520b1d7dec
Yulia <92887972+kachkovska@users.noreply.github.com>
2021-10-28 23:30:43 +0300

    Tasks to lecture 5

---
+++ TasksLec5.js
@@ -0,0 +1,65 @@
+console.log("Task 1");
+
+
+console.log("Task 2");
+
+function isBlank(someString) {
+    if (someString.length === 0 || someString.trim() == ""){
+        return 'Given string is blank';
+    } else {
+        return 'Given string is not blank';
+    }
+}
+console.log(isBlank(""));
+console.log(isBlank("     "));
+console.log(isBlank("Spam, eggs, bacon!"));
+
+console.log("Task 3");
+
+
+console.log("Task 4");
+
+let numb1 = Math.floor(Math.random()*200);
+let numb2 = Math.floor(Math.random()*200);
+
+function displayLargerNum(a,b) {
+    if (a > b) {
+        return `The larger of ${a} and ${b} is ${a}`;
+    } else if (a < b) {
+        return `The larger of ${a} and ${b} is ${b}`;
+    } else {
+        return `Numbers ${a} and ${b} are equal`;
+    }
+}
+console.log(displayLargerNum(numb1, numb2));
+
+console.log("Task 5");
+
+let num1 = Math.floor(Math.random()*200);
+let num2 = Math.floor(Math.random()*200);
+let num3 = Math.floor(Math.random()*200);
+
+function sortThreeNums(a, b, c){
+    if (a <= b && b <= c){
+        return `numbers ${a}, ${b}, ${c}, sorted in ascending order: ${a}, ${b}, ${c}`;
+    } else if (a <= c && c <= b) {
+        return `numbers ${a}, ${b}, ${c}, sorted in ascending order: ${a}, ${c}, ${b}`;
+    } else if (b <= a && a <= c) {
+        return `numbers ${a}, ${b}, ${c}, sorted in ascending order: ${b}, ${a}, ${c}`;
+    } else if (b <= c && c <= a) {
+        return `numbers ${a}, ${b}, ${c}, sorted in ascending order: ${b}, ${c}, ${a}`;
+    } else if (c <= a && a <= b) {
+        return `numbers ${a}, ${b}, ${c}, sorted in ascending order: ${c}, ${a}, ${b}`;
+    } else {
+        return `numbers ${a}, ${b}, ${c}, sorted in ascending order: ${c}, ${b}, ${a}`;
+    }
+}
+
+console.log(sortThreeNums(num1, num2, num3));
+/*
+
+
+
+c,a,b
+c,b,a
+*/
